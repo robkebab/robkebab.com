0a4662c2e4af6384f85faf0fd66f9d22246a63d4
Robert Collins <58490986+robkebab@users.noreply.github.com>
2023-09-03 16:20:35 -0500

    add TextLink

--- app/components/about/About.tsx
+++ app/components/about/About.tsx
@@ -1,43 +1,18 @@
 import { Heading } from "../heading/Heading";
 import { Paragraph } from "../paragraph/Paragraph";
 import { Section } from "../section/Section";
+import { TextLink } from "../text-link/TextLink";
+import { defaults } from "./defaults";
 import styles from "./About.module.css";
 
 export const About = () => {
   return (
     <Section id="about" className={styles.container}>
-      <Heading>About</Heading>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </Paragraph>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </Paragraph>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </Paragraph>
-      <a>
-        <span>Learn more about me</span>
-        <span>caret</span>
-      </a>
+      <Heading className={styles.heading}>About</Heading>
+      {defaults.paragraphs.map((p) => (
+        <Paragraph className={styles.paragraph} key={p.id}>{p.text}</Paragraph>
+      ))}
+      <TextLink href="#about">View full resume</TextLink>
     </Section>
   );
 };
