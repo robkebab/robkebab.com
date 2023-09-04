385be8450bed9a578d68847312a571975d5b3226
Robert Collins <58490986+robkebab@users.noreply.github.com>
2023-09-03 23:34:58 -0500

    change cards list back to div

--- app/components/experience/Experience.tsx
+++ app/components/experience/Experience.tsx
@@ -9,20 +9,19 @@ export const Experiences = () => {
   return (
     <Section id="experiences" className={styles.container}>
       <Heading className={styles.heading}>{defaults.title}</Heading>
-      <ul>
+      <div>
         {defaults.cards.map((c) => (
-          <li key={c.id}>
-            <ExperienceCard
-              description={c.description}
-              job={c.job}
-              jobLink={c.jobLink}
-              skills={c.skills}
-              timeline={c.timeline}
-              title={c.title}
-            />
-          </li>
+          <ExperienceCard
+            key={c.id}
+            description={c.description}
+            job={c.job}
+            jobLink={c.jobLink}
+            skills={c.skills}
+            timeline={c.timeline}
+            title={c.title}
+          />
         ))}
-      </ul>
+      </div>
       <TextLink href={defaults.link.href}>{defaults.link.text}</TextLink>
     </Section>
   );
